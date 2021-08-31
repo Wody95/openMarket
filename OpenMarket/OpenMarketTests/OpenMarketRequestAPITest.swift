5baee26df6bf49a9ed44b06b780061ad4cca12d4
wody95 <kwu0626@gmail.com>
2021-08-31 17:24:35 +0900

    test: Request API Model
    Request API Model
    - RequestCreateItem
    - RequestUpdateItem
    - RequestDeleteItem
    
    encoding Test Code 추가

---
+++ OpenMarket/OpenMarketTests/OpenMarketRequestAPITest.swift
@@ -0,0 +1,124 @@
+import XCTest
+
+@testable import OpenMarket
+
+class OpenMarketRequestAPITest: XCTestCase {
+    // Request API Model Test
+
+    // RequestCreateItem Test
+    func test_encode_RequestCreateItem() {
+        let newItme = RequestCreateItem(title: "newItem",
+                                        descriptions: "newItem_descriptions",
+                                        price: 10000,
+                                        currency: "KRW",
+                                        stock: 50000,
+                                        discountedPrice: 8000,
+                                        images: ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ATest-Logo.svg&psig=AOvVaw2M6t5dGULeeIpuP8akpheQ&ust=1630483537398000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOiKudPm2vICFQAAAAAdAAAAABAJ"],
+                                        password: "0000")
+
+        guard let _ = try? JSONEncoder().encode(newItme) else {
+            XCTFail()
+            return
+        }
+    }
+
+    func test_RequestCreateItem_discountedPrice_is_nil() {
+        let newItme = RequestCreateItem(title: "newItem",
+                                        descriptions: "newItem_descriptions",
+                                        price: 10000,
+                                        currency: "KRW",
+                                        stock: 50000,
+                                        discountedPrice: nil,
+                                        images: ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ATest-Logo.svg&psig=AOvVaw2M6t5dGULeeIpuP8akpheQ&ust=1630483537398000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOiKudPm2vICFQAAAAAdAAAAABAJ"],
+                                        password: "0000")
+
+        guard let _ = try? JSONEncoder().encode(newItme) else {
+            XCTFail()
+            return
+        }
+    }
+
+    func test_RequestCreateItem_password_is_empty() {
+        let newItme = RequestCreateItem(title: "newItem",
+                                        descriptions: "newItem_descriptions",
+                                        price: 10000,
+                                        currency: "KRW",
+                                        stock: 50000,
+                                        discountedPrice: nil,
+                                        images: ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ATest-Logo.svg&psig=AOvVaw2M6t5dGULeeIpuP8akpheQ&ust=1630483537398000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOiKudPm2vICFQAAAAAdAAAAABAJ"],
+                                        password: "")
+
+        guard let _ = try? JSONEncoder().encode(newItme) else {
+            XCTFail()
+            return
+        }
+    }
+
+    // RequestUpdateItem Test
+    func test_RequestUpdateItem_encode() {
+        let updateItem = RequestUpdateItem(title: "newItem",
+                                        descriptions: "newItem_descriptions",
+                                        price: 10000,
+                                        currency: "KRW",
+                                        stock: 50000,
+                                        discountedPrice: 6000,
+                                        images: ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ATest-Logo.svg&psig=AOvVaw2M6t5dGULeeIpuP8akpheQ&ust=1630483537398000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOiKudPm2vICFQAAAAAdAAAAABAJ"],
+                                        password: "")
+
+        guard let _ = try? JSONEncoder().encode(updateItem) else {
+            XCTFail()
+            return
+        }
+    }
+
+    func test_RequestUpdateItem_dicountedPrice_is_nil() {
+        let updateItem = RequestUpdateItem(title: "newItem",
+                                        descriptions: "newItem_descriptions",
+                                        price: 10000,
+                                        currency: "KRW",
+                                        stock: 50000,
+                                        discountedPrice: nil,
+                                        images: ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ATest-Logo.svg&psig=AOvVaw2M6t5dGULeeIpuP8akpheQ&ust=1630483537398000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOiKudPm2vICFQAAAAAdAAAAABAJ"],
+                                        password: "")
+
+        guard let _ = try? JSONEncoder().encode(updateItem) else {
+            XCTFail()
+            return
+        }
+    }
+
+    func test_RequestUpdateItem_password_is_empty() {
+        let updateItem = RequestUpdateItem(title: "newItem",
+                                        descriptions: "newItem_descriptions",
+                                        price: 10000,
+                                        currency: "KRW",
+                                        stock: 50000,
+                                        discountedPrice: nil,
+                                        images: ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ATest-Logo.svg&psig=AOvVaw2M6t5dGULeeIpuP8akpheQ&ust=1630483537398000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOiKudPm2vICFQAAAAAdAAAAABAJ"],
+                                        password: "")
+
+        guard let _ = try? JSONEncoder().encode(updateItem) else {
+            XCTFail()
+            return
+        }
+    }
+
+    // RequestDeleteItem Test
+    func test_RequestDeleteItem_encode() {
+        let deleteItem = RequestDeleteItem(password: "0000")
+
+        guard let _ = try? JSONEncoder().encode(deleteItem) else {
+            XCTFail()
+            return
+        }
+    }
+
+    func test_RequestDeleteItem_password_is_empty() {
+        let deleteItem = RequestDeleteItem(password: "")
+
+        guard let _ = try? JSONEncoder().encode(deleteItem) else {
+            XCTFail()
+            return
+        }
+    }
+}
--- OpenMarket/OpenMarketTests/OpenMarketTests.swift
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -4,8 +4,6 @@ import XCTest
 
 class OpenMarketTests: XCTestCase {
 
-
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -14,16 +12,8 @@ class OpenMarketTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_RequestCreateItem() {
+        
     }
 
 }
