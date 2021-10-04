4bda6784d4c95bde75f8348f46739be42a205073
wody95 <kwu0626@gmail.com>
2021-10-04 15:56:37 +0900

    #2 리스트와 그리드 전환버튼 및 상품 추가 버튼 UI 구현

--- OpenMarket/OpenMarket/ViewController/ViewController.swift
+++ OpenMarket/OpenMarket/ViewController/ViewController.swift
@@ -2,7 +2,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let rightSideView = RightSideView(frame: CGRect(x: 0, y: 0, width: 70, height: 37))
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupNavigationBar()
+    }
+
+    private func setupNavigationBar() {
+        self.navigationItem.title = "Market"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: rightSideView)
     }
 }
